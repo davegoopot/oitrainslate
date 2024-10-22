5644b6f1b14e067609df2bd4a6b766f36603be8a
Dave Potts <dave@goopot.co.uk>
2024-10-22 17:29:53 +0100

    First experiment pulling response.

--- tests/test_realtraintimes_api.py
+++ tests/test_realtraintimes_api.py
@@ -7,7 +7,23 @@ load_dotenv()
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
 
+def parse_response(response):
+    departures = []
+    for service in response['services']:
+        scheduled_time = service['locationDetail']['gbttBookedDeparture']
+        lateness_status = service['locationDetail']['lateness']
+        departures.append((scheduled_time, lateness_status))
+    return departures
+
 def test_realtraintimes_api_response():
     response = requests.get('https://api.rtt.io/api/v1/json/search/BWD', auth=(username, password))
     assert response.status_code == 200
-    print(response.json())
+    json_response = response.json()
+    assert 'services' in json_response
+    departures = parse_response(json_response)
+    assert isinstance(departures, list)
+    for departure in departures:
+        assert isinstance(departure, tuple)
+        assert len(departure) == 2
+        assert isinstance(departure[0], str)
+        assert isinstance(departure[1], int)
