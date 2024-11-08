5d01887076af5e85b0fd3c859e0b244d8ad730fc
Dave Potts <dave@davepotts.software>
2024-11-08 07:20:17 +0000

    Clean separation of API testing code. All the tests passing

--- tests/test_realtraintimes_api.py
+++ tests/test_realtraintimes_api.py
@@ -1,13 +1,37 @@
+"""
+    These test are for experimentally testing the realtraintimes API. They are
+    not part of the final implementation, but are useful for understanding how
+    the API works and how to parse the data.
+"""
+
 import os
 
 import requests
 from dotenv import load_dotenv
-from oitrainslate.realtraintimes import parse_response, TrainJourney
+
 
 load_dotenv()
 
 username = os.getenv('RTTUSERNAME')
 password = os.getenv('RTTPASSWORD')
+
+def parse_response(response):
+    journeys = []
+    for service in response['services']:
+        departure_station = service['locationDetail']['origin'][0]['tiploc']
+        departure_time = service['locationDetail']['gbttBookedDeparture']
+        destination = service['locationDetail']['destination'][0]['tiploc']
+        serviceuid = service['serviceUid']
+        rundate = service['runDate']
+        journeys.append({
+            "departure_station": departure_station,
+            "departure_time": departure_time,
+            "destination": destination,
+            "serviceuid": serviceuid,
+            "rundate": rundate,
+        })
+    return journeys
+
 
 def test_realtraintimes_api_response():
     response = requests.get('https://api.rtt.io/api/v1/json/search/BWD',
@@ -19,9 +43,8 @@ def test_realtraintimes_api_response():
     journeys = parse_response(json_response)
     assert isinstance(journeys, list)
     for journey in journeys:
-        assert isinstance(journey, TrainJourney)
-        assert isinstance(journey.departure_station, str)
-        assert isinstance(journey.departure_time, str)
-        assert isinstance(journey.stations, list)
-        for station in journey.stations:
-            assert isinstance(station, str)
+        assert isinstance(journey["departure_station"], str)
+        assert isinstance(journey["departure_time"], str)
+        assert isinstance(journey["destination"], str)
+        assert isinstance(journey["serviceuid"], str)
+        assert isinstance(journey["rundate"], str)
