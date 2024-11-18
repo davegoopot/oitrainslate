cc57d85645a0964223568002c4dfefbfa681ec06
Dave Potts <dave@davepotts.software>
2024-11-18 21:06:15 +0000

    Retrieve all the service information for all services.

--- oitrainslate/realtime_times_data_loader.py
+++ oitrainslate/realtime_times_data_loader.py
@@ -1,4 +1,7 @@
+import json
 import requests
+
+from oitrainslate import ServiceDetails
 
 class RealTimeTimesDataLoader:
     def __init__(self, username, password):
@@ -15,3 +18,25 @@ class RealTimeTimesDataLoader:
         if not response.ok:
             response.raise_for_status()
         return response.text
+    
+
+    def retrieveServicesFromSearch(self, departure_station_code: str, destination_station_code: str):
+        station_info = self.retrieveStationInformationJson(departure_station_code, destination_station_code)
+        parsed_info = json.loads(station_info)
+        services = parsed_info['services']
+        service_details = []
+        for service in services:
+            service_json = self.retrieveServiceJson(service['serviceUid'], service['runDate'])
+            service_details.append(ServiceDetails.create_from_json(json.dumps(service_json)))
+        return service_details
+    
+
+    def retrieveServiceJson(self, service_uid: str, run_date: str):
+        formatted_run_date = run_date.replace("-", "/")
+        service_url = f"https://api.rtt.io/api/v1/json/service/{service_uid}/{formatted_run_date}"
+        response = requests.get(service_url,
+                                auth=(self.username, self.password),
+                                timeout=10)
+        if not response.ok:
+            response.raise_for_status()
+        return response.json()
--- tests/test_realtime_times_data_loader.py
+++ tests/test_realtime_times_data_loader.py
@@ -1,8 +1,7 @@
-
 import json
 import os
 import pytest
-from oitrainslate import RealTimeTimesDataLoader
+from oitrainslate import RealTimeTimesDataLoader, ServiceDetails
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,16 +9,17 @@ load_dotenv()
 username = os.getenv('RTTUSERNAME')
 password = os.getenv('RTTPASSWORD')
 
-def test_retrieve_station_information_json():
-    data_loader = RealTimeTimesDataLoader(username, password)
+@pytest.fixture
+def data_loader():
+    return RealTimeTimesDataLoader(username, password)
+
+def test_retrieve_station_information_json(data_loader):
     station_code = 'MAN'
     result = data_loader.retrieveStationInformationJson(station_code)
     assert isinstance(result, str)
     assert result != ""
 
-
-def test_retrieve_station_information_json_filtered_by_to():
-    data_loader = RealTimeTimesDataLoader(username, password)
+def test_retrieve_station_information_json_filtered_by_to(data_loader):
     departure_station_code = "MAN"
     destination_station_code = "EUS"
     result = data_loader.retrieveStationInformationJson(departure_station_code, destination_station_code)
@@ -27,5 +27,13 @@ def test_retrieve_station_information_json_filtered_by_to():
     destinations = [service["locationDetail"]["destination"][0]["tiploc"] for service in parsed_result["services"]]
 
     assert all(destination == "EUSTON" for destination in destinations)
+
+def test_retrieve_services_from_search(data_loader):
+    departure_station_code = "BWD"
+    destination_station_code = "URM"
+    results = data_loader.retrieveServicesFromSearch(departure_station_code, destination_station_code)
+    assert isinstance(results, list)
+    assert len(results) > 0
+    assert all(isinstance(result, ServiceDetails) for result in results)
 
 
